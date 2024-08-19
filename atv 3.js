291b0ada1834429af589d3e5236d796dcbad637e
CauaCanale <caua.cferreira2@gmail.com>
2024-08-19 00:43:21 -0300

    Update atv 3.js

--- atv 3.js
+++ atv 3.js
@@ -1,7 +1,11 @@
-num=1
-soma=0
-while(num!=0){  
- num=parseInt(prompt("Digite um número."))  
- 	 soma = num + soma
+function total (num, soma){
+num = 1
+soma = 0
+
+while (num!=0){  
+ num = parseInt (prompt("Digite um número."))  
+   soma = num + soma
 }
-console.log("Resultado: " +soma)
+   alert("Resultado: " + soma) //A variavel "soma" é para aparecer a soma dos números digitados no "Prompt", A "soma" guarda o resultado dos numeros.
+}
+   total ()
